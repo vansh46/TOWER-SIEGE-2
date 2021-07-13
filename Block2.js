238ec17aa69b687df4addd4f13ba9444a8be5355
vansh suthar <78260154+vansh46@users.noreply.github.com>
2021-07-13 16:34:10 +0530

    Add files via upload

---
+++ Block2.js
@@ -0,0 +1,35 @@
+class Block2{
+ 
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.4,
+            'friction':0.1
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.visibility = 230;
+        
+        World.add(world, this.body);
+      }
+      display(){
+        console.log(this.body.speed);
+        if(this.body.speed <10){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill(rgb(240,128,128));
+        rect(0,0,this.width, this.height);
+        pop();
+      }else{
+        World.remove(world, this.body);
+        push();
+        this.visibility = this.visibility -5;
+        pop();
+        
+      }
+    }
+  }
